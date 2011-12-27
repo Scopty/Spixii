6172f307ac719945c14f6d63cda7f793f2a1aa74
Per Karlsson <naturalideas@gmail.com>
2011-12-27 21:31:14 +0000

    added new stuff to dbQuery script file.

---
+++ Scripts/dbFunc/dbQuery.cs
@@ -0,0 +1,12 @@
+//dbQuery.cs
+/*
+SUMMARY of this function goes here
+----------------------------------
+- This function takes an SQL argument and returns the output as a string.
+
+
+DETAILED explanation goes here
+------------------------------
+
+
+*/
